1291e981b896d715c6192245b75db8b82e0bfb27
vasukimurugesam111-bit <vasukimurugesam111@gmail.com>
2026-08-14 13:16:48 +0530

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+    public static void showMenu() {
+
+        System.out.println("\n====== BANK MANAGEMENT SYSTEM ======");
+        System.out.println("1. Create Account");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Check Balance");
+        System.out.println("5. Display Account");
+        System.out.println("6. Exit");
+        System.out.print("Enter Your Choice : ");
+    }  
+}
